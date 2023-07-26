4c669bbc18145728c6bd7cf0a533eea1ff2eb3c1
Artan <artanpl03@gmail.com>
2023-07-25 22:40:04 -0400

    basic slider with 4 renders

--- bodyboost/src/components/Commitment.js
+++ bodyboost/src/components/Commitment.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Commitment = () => {
+  const [value, setValue] = useState(1);
+
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    console.log('Slider value:', newValue); 
+    setValue(newValue);
+  };
+
   return (
-    <h1>Commitment</h1>
-  )
+    <div>
+      <h1>Commitment Level</h1>
+      <input
+        type="range"
+        min="1"
+        max="4"
+        value={value}
+        onChange={handleChange}
+        className="slider"
+      />
+      {value >= 1 && <p>Current Level: Lvl 1: Exercise Tracking</p>}
+      {value >= 2 && <p>+ Level 2: Weight Tracking</p>}
+      {value >= 3 && <p>+ Level 3: Photo Progress</p>}
+      {value >= 4 && <p>+ Contract</p>}
+    </div>
+  );
 }
+
 
 export default Commitment
