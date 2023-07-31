6d3cf72c8301fc612a956c154381c19b3119a7e8
Artan <artanpl03@gmail.com>
2023-07-31 01:23:42 -0400

    added accordion for contract

--- bodyboost/src/components/Commitment.js
+++ bodyboost/src/components/Commitment.js
@@ -5,13 +5,17 @@ import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import '../styles/Commitment.css'
 import SignatureCanvas from 'react-signature-canvas';
+import { Collapse } from 'antd';
 
+
+const { Panel } = Collapse;
 
 const Commitment = () => {
-    const [content, setContent] = useState('');
+    const [content, setContent] = useState('I, [Your Name], commit to achieving my personal goals...');
     const [submittedContent, setSubmittedContent] = useState("");
     const [signatureImage, setSignatureImage] = useState("");
     const [contracts, setContracts] = useState([]);
+    const [confirmationMessage, setConfirmationMessage] = useState('');
     const sigCanvas = useRef({});
 
 //---------------------------------------------------------------------------------------
@@ -85,7 +89,8 @@ const handleContentChange = (value) => {
             console.log(`get req: ${response.data}`);
     
             setSubmittedContent(contractResponse.data.content);
-            setSignatureImage(contractResponse.data.signature); 
+            setSignatureImage(contractResponse.data.signature);
+            alert('Your contract has been saved.');
         } catch (error) {
             console.error(error);
         }
@@ -128,11 +133,20 @@ const handleContentChange = (value) => {
 
       return (
         <div className='contract-container'>
-            <div className="quill-wrapper">
-                <ReactQuill value={content} onChange={handleContentChange} modules={modules} className="editor"/>
-            </div>
-            <SignatureCanvas ref={sigCanvas} canvasProps={{width: 400, height: 100, className: 'sigCanvas', style: {backgroundColor: 'rgba(0,0,0,0.1)'}}} />
-            <button onClick={saveContract} className="save-button">Save Contract</button>
+          <h2>Commitment Contract</h2>
+           <p>Write your personal commitment contract here. This is a contract with yourself to achieve your personal goals.</p>
+            <Collapse accordion>
+                <Panel header="Create Contract" key="1">
+                    <div className="quill-wrapper">
+                        <ReactQuill value={content} onChange={handleContentChange} modules={modules} className="editor"/>
+                    </div>
+                    <div className="signature-section">
+                      <h2>Signature</h2>
+                      <SignatureCanvas ref={sigCanvas} canvasProps={{width: 400, height: 100, className: 'sigCanvas', style: {backgroundColor: 'rgba(0,0,0,0.1)'}}} />
+                    </div>
+                    <button onClick={saveContract} className="save-button">Save Contract</button>
+                </Panel>
+            </Collapse>
             <div className="content-wrapper">
                 <div className="contract-content" dangerouslySetInnerHTML={{ __html: submittedContent }} />
             </div>
@@ -140,7 +154,10 @@ const handleContentChange = (value) => {
             {contracts.map(contract => (
             <div key={contract._id} className="content-wrapper">
                 <div className="contract-content" dangerouslySetInnerHTML={{ __html: contract.content }} />
-                <img src={contract.signature} alt="Signature" />
+                <div className="signature-display">
+                  <h2>Signature:</h2>
+                  <img src={contract.signature} alt="Signature" />
+                </div>
                 <button onClick={() => deleteContract(contract._id)}>Delete</button>
             </div>
             ))}
