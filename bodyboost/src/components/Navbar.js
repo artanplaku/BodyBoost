463ac2afb69bd33be776601272482c1f484c4476
Artan <artanpl03@gmail.com>
2023-08-06 22:59:33 -0400

    styling changes for navbar/dark

--- bodyboost/src/components/Navbar.js
+++ bodyboost/src/components/Navbar.js
@@ -4,11 +4,14 @@ import '../styles/Navbar.scss'
 import { AuthContext } from '../contexts/AuthContext';
 import LanguageSelector from './LanguageSelector';
 import { useTranslation } from 'react-i18next';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 
 const Navbar = () => {
   const { t } = useTranslation();
   const { isLoggedIn, setLoggedIn } = useContext(AuthContext);
+  const { isDarkMode } = useContext(ThemeContext);
+
   const navigate = useNavigate();
   
   
@@ -23,16 +26,25 @@ const Navbar = () => {
     <nav className="navbar">
       <LanguageSelector />
     <ul>
-      <li><Link to="/home">{t('navbar.home')}</Link></li>
-      <li><Link to="/workouts">{t('navbar.workouts')}</Link></li>
-      <li><Link to="/exercises">{t('navbar.exercises')}</Link></li>
-      <li><Link to="/history">{t('navbar.history')}</Link></li>
-      <li><Link to="/progress">{t('navbar.progress')}</Link></li>
-      <li><Link to="/settings">{t('navbar.settings')}</Link></li>
-      <li><Link to="/commitment">{t('navbar.commitment')} </Link></li>
-      <li><Link to="/challenges">{t('navbar.challenges')}</Link></li>
-      <li><Link to="/achievements">{t('navbar.achievements')}</Link></li>
-      {isLoggedIn && <li><button onClick={handleLogout}>{t('navbar.logout')}</button></li>}
+      <li>
+        <Link to="/home" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.home')}</Link></li>
+      <li>
+        <Link to="/workouts" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.workouts')}</Link></li>
+      <li>
+        <Link to="/exercises" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.exercises')}</Link></li>
+      <li>
+        <Link to="/history" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.history')}</Link></li>
+      <li>
+        <Link to="/progress" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.progress')}</Link></li>
+      <li>
+        <Link to="/settings" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.settings')}</Link></li>
+      <li>
+        <Link to="/commitment" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.commitment')} </Link></li>
+      <li>
+        <Link to="/challenges" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.challenges')}</Link></li>
+      <li>
+        <Link to="/achievements" className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.achievements')}</Link></li>
+      {isLoggedIn && <li><button onClick={handleLogout} className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>{t('navbar.logout')}</button></li>}
         {!isLoggedIn && <li><Link to="/login">{t('navbar.login')}</Link></li>}
     </ul>
   </nav>
