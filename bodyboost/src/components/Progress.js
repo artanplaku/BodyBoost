350f283b6b1f1cdbe93e3028e5658564d544cb08
Artan <artanpl03@gmail.com>
2023-08-07 22:24:45 -0400

    added image carousel and dropdown

--- bodyboost/src/components/Progress.js
+++ bodyboost/src/components/Progress.js
@@ -3,10 +3,14 @@ import jwtDecode from 'jwt-decode';
 import axios from 'axios';
 import "../styles/Progress.scss"
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight, faChevronDown, faUpload, faFile } from '@fortawesome/free-solid-svg-icons';
 
 function Progress() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [images, setImages] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -79,24 +83,75 @@ function Progress() {
             },
         });
         fetchImages();  // Refresh the images after deletion
+        if (currentIndex >= images.length - 1) { 
+          setCurrentIndex(images.length - 2);
+      }
     } catch (error) {
         console.error(error);
     }
 };
 
+const nextImage = () => {
+  if (currentIndex === images.length - 1) {
+    setCurrentIndex(0);
+  } else {
+    setCurrentIndex(currentIndex + 1);
+  }
+};
 
-  return (
-    <div className="container">
-    <input type="file" onChange={handleFileChange} />
-    <button onClick={uploadFile}>{t('progress.upload')}</button>
-    {images.map((imageData, index) => (
-  <div key={imageData._id} className="image-wrapper">
-    <img src={imageData.dataUrl} alt={`img-${index}`} className="image" />
-    <button onClick={() => deleteImage(imageData._id)}>Delete</button>
-  </div>
-))}
+const prevImage = () => {
+  if (currentIndex === 0) {
+    setCurrentIndex(images.length - 1);
+  } else {
+    setCurrentIndex(currentIndex - 1);
+  }
+};
+
+
+
+return (
+  <div className="container">
+    <div className="dropdown-container">
+
+   <button className="dropdown-trigger" onClick={() => setShowDropdown(prev => !prev)}>
+  Add a Picture <FontAwesomeIcon icon={faChevronDown} />
+</button>
+    {showDropdown && (
+      <div className="dropdown-content">
+        <div className="file-input-wrapper">
+          <label htmlFor="fileInput" className="customFileInput">
+            <FontAwesomeIcon icon={faFile} /> Choose a File
+          </label>
+          <input type="file" id="fileInput" onChange={handleFileChange} style={{ display: 'none' }} />
+        </div>
+        <button onClick={uploadFile} disabled={!selectedFile}>
+          <FontAwesomeIcon icon={faUpload} /> {t('progress.upload')}
+        </button>
+      </div>
+    )}
+    </div>
+
+    {images[currentIndex] && (
+      <div className="image-wrapper">
+        <img src={images[currentIndex].dataUrl} alt={`img-${currentIndex}`} className="image" />
+        <button className="delete-btn" onClick={() => deleteImage(images[currentIndex]._id)}>Delete</button>
+      </div>
+    )}
+    
+    <div className="carousel-indicators">
+      {images.map((_, index) => (
+        <span key={index} className={`indicator ${index === currentIndex ? 'active' : ''}`} onClick={() => setCurrentIndex(index)}></span>
+      ))}
+    </div>
+
+    <div className="carousel-navigation">
+    <button onClick={prevImage}><FontAwesomeIcon icon={faChevronLeft} /> Prev</button>
+    <button onClick={nextImage}>Next <FontAwesomeIcon icon={faChevronRight} /></button>
+    </div>
+
   </div>
 );
+
 }
 
 export default Progress;
