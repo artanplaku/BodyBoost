717174f4d951ab17ead8ffcf99004311ab6ea229
Artan <artanpl03@gmail.com>
2023-08-26 23:09:08 -0400

    responsive navbar

---
+++ bodyboost/src/components/NavLinks.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faHome,
+  faDumbbell,
+  faHistory,
+  faChartLine,
+  faCog,
+  faTrophy, 
+  faMedal, 
+  faSignInAlt, 
+  faSignOutAlt, 
+  faMountain,
+  faHeartbeat,
+ } 
+ from '@fortawesome/free-solid-svg-icons';
+
+const NavLinks = ({ isDarkMode, isLoggedIn, handleLogout, t }) => {
+  return (
+    <div>
+           <ul>
+      <li>
+        <Link to="/home" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faHome} className='icon'/> 
+          {t('navbar.home')}
+          </Link>
+        </li>
+      <li>
+        <Link to="/workouts" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faDumbbell} className='icon'/> 
+          {t('navbar.workouts')}
+          </Link>
+      </li>
+      <li>
+        <Link to="/exercises" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faHeartbeat} className='icon'/>
+          {t('navbar.exercises')}
+          </Link>
+          </li>
+      <li>
+        <Link to="/history" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faHistory} className='icon'/> 
+          {t('navbar.history')}
+          </Link>
+          </li>
+      <li>
+        <Link to="/progress" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+            <FontAwesomeIcon icon={faChartLine} className='icon'/>
+          {t('navbar.progress')}
+          </Link>
+          </li>
+      <li>
+        <Link to="/settings" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faCog} className='icon'/>
+          {t('navbar.settings')}
+          </Link>
+          </li>
+      <li>
+        <Link to="/commitment" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faMedal} className='icon'/>
+          {t('navbar.commitment')} 
+          </Link>
+          </li>
+      <li>
+        <Link to="/challenges" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+            <FontAwesomeIcon icon={faMountain} className='icon'/>
+          {t('navbar.challenges')}
+          </Link>
+          </li>
+      <li>
+        <Link to="/achievements" 
+          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+          <FontAwesomeIcon icon={faTrophy} className='icon'/>
+          {t('navbar.achievements')}
+          </Link>
+          </li>
+
+      {isLoggedIn && 
+       <li>
+       <button 
+        onClick={handleLogout} 
+        className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
+        <FontAwesomeIcon icon={faSignOutAlt} className='icon'/>
+        {t('navbar.logout')}
+      </button>
+      </li>}
+
+      {!isLoggedIn &&
+       <li>
+        <Link to="/login">
+        <FontAwesomeIcon icon={faSignInAlt} className='icon'/>
+          {t('navbar.login')}
+          </Link>
+          </li>}
+    </ul>
+    </div>
+  )
+}
+
+export default NavLinks
--- bodyboost/src/components/Navbar.js
+++ bodyboost/src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import {  useNavigate } from 'react-router-dom';
 import '../styles/Navbar.scss'
 import { AuthContext } from '../contexts/AuthContext';
 // import LanguageSelector from './LanguageSelector';
@@ -7,25 +7,21 @@ import { useTranslation } from 'react-i18next';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
-  faHome,
-  faDumbbell,
-  faHistory,
-  faChartLine,
-  faCog,
-  faTrophy, 
-  faMedal, 
-  faSignInAlt, 
-  faSignOutAlt, 
-  faMountain,
-  faHeartbeat,
- } 
-from '@fortawesome/free-solid-svg-icons';
+  faBars,
+ } from '@fortawesome/free-solid-svg-icons';
+ import NavLinks from './NavLinks';
+
 
 
 const Navbar = () => {
   const { t } = useTranslation();
   const { isLoggedIn, setLoggedIn } = useContext(AuthContext);
   const { isDarkMode } = useContext(ThemeContext);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
 
   const navigate = useNavigate();
   
@@ -38,92 +34,33 @@ const Navbar = () => {
   
   
   return (
+    <>
+     <div className="hamburger" onClick={toggleModal}>
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+
     <nav className="navbar">
-      {/* <LanguageSelector /> */}
-    <ul>
-      <li>
-        <Link to="/home" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faHome} className='icon'/> 
-          {t('navbar.home')}
-          </Link>
-        </li>
-      <li>
-        <Link to="/workouts" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faDumbbell} className='icon'/> 
-          {t('navbar.workouts')}
-          </Link>
-      </li>
-      <li>
-        <Link to="/exercises" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faHeartbeat} className='icon'/>
-          {t('navbar.exercises')}
-          </Link>
-          </li>
-      <li>
-        <Link to="/history" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faHistory} className='icon'/> 
-          {t('navbar.history')}
-          </Link>
-          </li>
-      <li>
-        <Link to="/progress" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-            <FontAwesomeIcon icon={faChartLine} className='icon'/>
-          {t('navbar.progress')}
-          </Link>
-          </li>
-      <li>
-        <Link to="/settings" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faCog} className='icon'/>
-          {t('navbar.settings')}
-          </Link>
-          </li>
-      <li>
-        <Link to="/commitment" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faMedal} className='icon'/>
-          {t('navbar.commitment')} 
-          </Link>
-          </li>
-      <li>
-        <Link to="/challenges" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-            <FontAwesomeIcon icon={faMountain} className='icon'/>
-          {t('navbar.challenges')}
-          </Link>
-          </li>
-      <li>
-        <Link to="/achievements" 
-          className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-          <FontAwesomeIcon icon={faTrophy} className='icon'/>
-          {t('navbar.achievements')}
-          </Link>
-          </li>
+    <NavLinks 
+      isDarkMode={isDarkMode} 
+      isLoggedIn={isLoggedIn} 
+      handleLogout={handleLogout} 
+      t={t} 
+      />
 
-      {isLoggedIn && 
-       <li>
-       <button 
-        onClick={handleLogout} 
-        className={`glow-button ${isDarkMode ? 'glow-button-dark' : ''}`}>
-        <FontAwesomeIcon icon={faSignOutAlt} className='icon'/>
-        {t('navbar.logout')}
-      </button>
-      </li>}
-
-      {!isLoggedIn &&
-       <li>
-        <Link to="/login">
-        <FontAwesomeIcon icon={faSignInAlt} className='icon'/>
-          {t('navbar.login')}
-          </Link>
-          </li>}
-    </ul>
+    {isModalOpen && (
+      <div className="modal-backdrop" onClick={toggleModal}>
+      <div className="modal">
+      <NavLinks 
+            isDarkMode={isDarkMode} 
+            isLoggedIn={isLoggedIn} 
+            handleLogout={handleLogout} 
+            t={t} 
+            />
+         </div>
+           </div>
+           )}
   </nav>
+  </>
   )
 }
 
