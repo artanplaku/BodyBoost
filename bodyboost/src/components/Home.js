46a8f1ca8271645cd445eca19e58926e222df5f0
Artan <artanpl03@gmail.com>
2023-09-01 23:57:45 -0400

    styling changes

--- bodyboost/src/components/Home.js
+++ bodyboost/src/components/Home.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import History from './History';
-
+import '../styles/Home.scss';
 
 const Home = () => {
   const { t } = useTranslation();
+  
   return (
     <div>
-      <div>
-      <h1>{t('welcome_message')}</h1>
+      <div className='welcomeContainer'>
+        <h1>{t('welcome_message')}</h1>
+        <span className='neonText'>BodyBoost</span>
+      </div>
       <p>{t('description_message')}</p>
       <History />
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
