78cbd2e49d055200449f44e39d96110899439948
Artan <artanpl03@gmail.com>
2023-09-08 14:23:48 -0400

    styling fixes

--- bodyboost/src/components/Home.js
+++ bodyboost/src/components/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import History from './History';
 import '../styles/Home.scss';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Home = () => {
   const { t } = useTranslation();
+  const { isDarkMode } = useContext(ThemeContext);
   
   return (
     <div>
       <div className='welcomeContainer'>
         <h1>{t('welcome_message')}</h1>
-        <span className='neonText'>BodyBoost</span>
+        <span className={`${isDarkMode? "darkNeon":"neonText"}`}>BodyBoost</span>
       </div>
       <p>{t('description_message')}</p>
       <History />
