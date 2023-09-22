a41401cc1c7e428233ce5ac0452a7b83fe74866a
Artan <artanpl03@gmail.com>
2023-09-21 23:45:57 -0400

    styling changes for landing

--- bodyboost/src/components/Landing.js
+++ bodyboost/src/components/Landing.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import "../styles/Landing.scss"
+import topLeftImage from "../assets/images/exercises1.png"
+import cloud from "../assets/images/cloud.png"
 
 
 const Landing = () => {
   return (
     <div className="landing-page">
-      <img src="image.jpg" alt="Top Left" className="top-left-image" />
-      <img src="image.jpg" alt="Top Right" className="top-right-image" />
-      <img src="image.jpg" alt="Below Top Right" className="below-top-right-image" />
-
+      <img src={cloud} alt="Top Left" className="top-left-image" />
+      <img src={cloud} alt="Top Right" className="top-right-image" />
       <div className="title-description">
-        <h1 className="website-name">Bodyboost</h1>
-        <p className="description">My description here...</p>
+      <h1 className="website-name">Body<span>boost</span></h1>
+        <p className="description">Where strength meets motivation</p>
       </div>
+      {/* <img src={cloud} alt="Below Top Right" className="below-top-right-image" /> */}
+
 
       <div className="footer-section">
+      <img src={topLeftImage} alt="Description " className="above-start-image" />
         <button className="start-button">Start</button>
         <p className="login-prompt">
           Already have an account? <button className="login-button">Login</button>
